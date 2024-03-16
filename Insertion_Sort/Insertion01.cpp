977f9b2a1bc8ba953db2fe208ee5bc984252a86e
Prashant Jinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-03-16 16:31:50 +0530

    Add Commit

---
+++ Insertion_Sort/Insertion01.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+void InsertionSort(int arr[], int size){
+
+    for(int i = 1; i < size; i++){
+    	int temp = arr[i];
+    	int j = i - 1;
+    	for(; j >= 0; j--){
+    		if(arr[j] > temp){
+    			arr[j+1] = arr[j];
+			}else{
+				break;
+			}
+		}
+		arr[j+1] = temp;
+	}
+    
+}
+
+int main(){
+    
+    int arr[6] = {1,5,8,4,3,6};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+
+    InsertionSort( arr,  size);
+
+    for (int i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+    
+
+    return 0;
+}
